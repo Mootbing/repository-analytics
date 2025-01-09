65f9209738d486060e8fcfd81273b0f93db92f0e
Jason Xu <50122069+Mootbing@users.noreply.github.com>
2025-01-08 23:33:35 -0500

    a

---
+++ Analysis.py
@@ -0,0 +1,110 @@
+import os
+import shutil
+from git import Repo # pip install gitpython
+import time
+from git import RemoteProgress
+
+# get directory to this file
+dir = os.path.dirname(os.path.realpath(__file__))   
+repo_path = os.path.join(dir, 'Repo')
+clone_repo_y_n = input('Clone a repository? (y/n): ')
+
+if clone_repo_y_n == 'y':
+
+    repo_url = input('Enter the URL of the repository: ')
+
+    # clear dir /Repo
+    def clear_dir():
+        print('[Log] Clearing previous directory...')
+        shutil.rmtree(repo_path)
+
+    # clone repo
+    class CloneProgress(RemoteProgress):
+        def update(self, op_code, cur_count, max_count=None, message=''):
+            if message:
+                print(message)
+
+    def clone_repo(repo_url):
+        print('[Log] Cloning repository...')
+        Repo.clone_from(repo_url, repo_path, progress=CloneProgress())
+        print('[Log] Repository cloned successfully')
+
+    os.mkdir(repo_path)
+
+    clone_repo(repo_url)
+
+# .ext : {linecount, filecount}
+fileExtensionsCounter = {}
+
+default = {
+    "line count": 0,
+    "file count": 0
+}
+
+# analysis
+def recursiveAnalytics(repo_path):
+    for root, dirs, files in os.walk(repo_path):
+        for file in files:
+            # exclude files and directories beginning with a .
+            if file.startswith('.') or root.startswith('.'):
+                continue
+
+            # get file extension
+            ext = file.split('.')[-1]
+
+            # count file extensions
+            if ext in fileExtensionsCounter:
+                fileExtensionsCounter[ext]["file count"] += 1
+            else:
+                fileExtensionsCounter[ext] = default.copy()
+                fileExtensionsCounter[ext]["file count"] += 1
+
+            # count lines of code
+            with open(os.path.join(root, file), 'r') as f:
+
+                try: 
+                    file_contents = f.readlines()
+                    fileExtensionsCounter[ext]["line count"] += len(file_contents)
+                except:
+                    print("[Error] Could not analyze file: " + os.path.join(root, file))
+                    continue
+
+            print("[Analyzing] " + os.path.join(root, file))
+
+def prune_last_results_md():
+    if os.path.exists(os.path.join(dir, 'results.md')):
+        os.remove(os.path.join(dir, 'results.md'))
+
+prune_last_results_md()
+
+recursiveAnalytics(repo_path)
+
+def fileExtensionsSortedByFileCount():
+    return list(sorted(fileExtensionsCounter.items(), key=lambda x: x[1]["file count"], reverse=True))
+
+def fileExtensionsSortedByLineCount():
+    return list(sorted(fileExtensionsCounter.items(), key=lambda x: x[1]["line count"], reverse=True))
+
+# write results to file
+def writeResults():
+    with open(os.path.join(dir, 'Analytics.md'), 'w+') as f:
+        f.write("# File Type Analytics\n")
+        f.write("## # of Files:\n" + str(len(fileExtensionsCounter)) + "\n\n")
+
+        f.write("## # of Files Per Extension:\n")
+        for ext in fileExtensionsSortedByFileCount():
+            f.write(f".{ext[0]} : {ext[1]['file count']}\n\n")
+
+        total_lines = 0
+        for ext in fileExtensionsCounter:
+            total_lines += fileExtensionsCounter[ext]["line count"]
+
+        f.write("\n## # of Lines Per Extension: \n")
+        for ext in fileExtensionsSortedByLineCount():
+            f.write(f".{ext[0]} : {ext[1]['line count']}\n\n")
+
+        f.write("\n## Total File Lines: \n")
+        f.write(str(total_lines))
+
+writeResults()
+print("[Log] Results written to file: Analytics.md")
---
+++ Analytics.md
@@ -0,0 +1,82 @@
+# File Type Analytics
+## # of Files:
+18
+
+## # of Files Per Extension:
+.jpg : 10
+
+.tsx : 9
+
+.ts : 6
+
+.js : 3
+
+.json : 3
+
+.md : 3
+
+.HEAD : 2
+
+.LICENSE : 1
+
+.config : 1
+
+.description : 1
+
+.index : 1
+
+.packed-refs : 1
+
+.idx : 1
+
+.pack : 1
+
+.rev : 1
+
+.master : 1
+
+.yml : 1
+
+.png : 1
+
+
+## # of Lines Per Extension: 
+.tsx : 3053
+
+.md : 719
+
+.js : 445
+
+.ts : 245
+
+.packed-refs : 176
+
+.json : 102
+
+.LICENSE : 21
+
+.config : 13
+
+.yml : 12
+
+.HEAD : 2
+
+.description : 1
+
+.master : 1
+
+.index : 0
+
+.idx : 0
+
+.pack : 0
+
+.rev : 0
+
+.jpg : 0
+
+.png : 0
+
+
+## Total File Lines: 
+4790
