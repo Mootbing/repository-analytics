094b1c1f95d453328ce6a96719f95a12707636d5
Jason Xu <50122069+Mootbing@users.noreply.github.com>
2025-01-08 23:41:40 -0500

    Initialize React application with basic structure and configuration

---
+++ Web/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <svg width={467} height={195}>
+      <title id="title">{name}</title>
+    </svg>
+  );
+}
+
+export default App;
